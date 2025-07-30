312bd70a29336e38d87b58921509b7bc3709c7fd
ShubhamkrTiwari <skt916606@gmail.com>
2025-07-31 01:02:37 +0530

    new

--- app/src/main/java/com/example/nexicraft/CardView.kt
+++ app/src/main/java/com/example/nexicraft/CardView.kt
@@ -2,25 +2,20 @@ package com.example.nexicraft
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageView
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.nexicraft.R.id
-import com.example.nexicraft.R.id.aboutus
+import com.example.nexicraft.R.*
 
 class CardView : AppCompatActivity() {
-    private fun <T> findViewById(): ImageView? {
-        TODO("Not yet implemented")
-    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_card_view)
-        val aboutus = findViewById<ImageView>(aboutus)
-        aboutus.setOnClickListener {
+        setContentView(layout.activity_card_view)
+        val aboutbutton: Button = findViewById(R.id.aboutbutton)
+        aboutbutton.setOnClickListener {
             val intent = Intent(this, aboutus::class.java)
             startActivity(intent)
         }
